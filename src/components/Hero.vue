e17de0fdc3ce0b007dff7170d69d4a8deb0f7218
Max Pares <urkytsk3@gmail.com>
2025-05-21 17:56:52 +0300

    Обновил заголовок услуги в компоненте Hero для улучшения читаемости: заменил 'EVO&nbsp;и&nbsp;3D&nbsp;ковры' на 'EVO и 3D ковры / органайзеры'.

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -78,7 +78,7 @@ const services = [
   },
   {
     id: 7,
-    title: 'EVO&nbsp;и&nbsp;3D&nbsp;ковры',
+    title: 'EVO и 3D ковры / органайзеры',
     description: 'Изготовление современных ковров с высоким бортом и органайзеров',
     image: '/images/carpets.svg',
     link: '/services/carpets',
