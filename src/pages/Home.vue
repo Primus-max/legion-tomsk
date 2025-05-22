cdf3db4536684c61673ee542f4a85b5af38f1039
Max Pares <urkytsk3@gmail.com>
2025-05-22 15:14:59 +0300

    Добавлен компонент FooterSection на главную страницу для улучшения структуры и навигации сайта.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,12 +4,14 @@
     <PortfolioSection />
     <PriceSection />
     <ReviewsSection />
+    <FooterSection />
     <h1 style="display:none">Главная</h1>
     <p style="display:none">Добро пожаловать на сайт по перетяжке, тюнингу и реставрации авто!</p>
   </main>
 </template>
 
 <script setup>
+import FooterSection from '../components/sections/FooterSection.vue';
 import HeroSection from '../components/sections/HeroSection.vue';
 import PortfolioSection from '../components/sections/PortfolioSection.vue';
 import PriceSection from '../components/sections/PriceSection.vue';
